5c887632129b9d747c7035131b01c05b3db584fc
James <james.rosstwo@gmail.com>
2021-01-16 15:02:56 -0800

    add API

--- src/assertion_finder.py
+++ src/assertion_finder.py
@@ -1,10 +1,26 @@
-import stanfordnlp
-from src.definitions import ROOT_PATH
+from src.definitions import ROOT_PATH, SETTINGS
+import requests
 
 
 class AssertionFinder:
     def __init__(self):
-        self.nlp = stanfordnlp.Pipeline(processors='tokenize', lang="en")
+        import requests
+
+        url = "https://idir.uta.edu/claimbuster/api/v2/score/text/Hello%20there"
+
+        headers = {
+            'x-api-key': SETTINGS["assertion_finder"]["api_key"],
+            'Accept': "*/*",
+            'Cache-Control': "no-cache",
+            'Host': "idir.uta.edu",
+            'Accept-Encoding': "gzip, deflate",
+            'Connection': "keep-alive",
+            'cache-control': "no-cache"
+        }
+
+        response = requests.request("GET", url, headers=headers)
+
+        print(response.text)
 
     def assertion_likelihood(self, sentence: str) -> float:
         return 0.5
